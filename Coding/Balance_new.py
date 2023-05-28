294c56250bb2e6d7c5e072bcc508014c9010fcae
EnricoGiachetto <enrico.giachetto@rationence.eu>
2023-05-29 01:34:09 +0200

    Update Balance_new.py

--- Coding/Balance_new.py
+++ Coding/Balance_new.py
@@ -9,22 +9,31 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import pandas as pd
 import time
+from tqdm import tqdm
+import re
 
 
 driver=webdriver.Firefox()
 fataframes=[]
-azioni=["AAPL", "MSFT", "AMZN", "GOOGL", "META", "TSLA", "NVDA", "PYPL", "INTC", "CMCSA",
-    "NFLX", "PEP", "ADBE", "CSCO", "AVGO", "TXN", "COST", "TMUS", "AMGN", "QCOM",
-    "CHTR", "SBUX", "BKNG", "AMD", "INTU", "ISRG", "GILD", "VRTX", "REGN", "MDLZ",
-    "ZM", "MU", "ATVI", "ADP", "MRNA", "ILMN", "CSX", "ADI", "ASML", "IDXX", "FISV",
-    "MELI", "WDAY", "KLAC", "LRCX", "NXPI", "BIIB", "MAR", "EXC", "ALGN", "CTSH"] 
+azioni=["ACRE","ACRO", "ACRS", "ACRV", "ACRX", "ACST", "ACT", "ACTG",
+    "ACU", "ACV", "ACVA", "ACXP", "ADAG", "ADAP", "ADBE", "ADC", "ADC^A", "ADCT",
+    "ADD", "ADEA", "ADER", "ADERW", "ADES", "ADEX", "ADI", "ADIL", "ADILW", "ADM",
+    "ADMA", "ADMP", "ADN", "ADNT", "ADNWW", "ADOC", "ADOCR", "ADOCW", "ADP", "ADPT",
+    "ADRT", "ADSE", "ADSEW", "ADSK", "ADT", "ADTH", "ADTHW", "ADTN", "ADTX", "ADUS",
+    "ADV", "ADVM", "ADVWW", "ADX", "ADXN", "AE", "AEAE", "AEAEW", "AEE", "AEF",
+    "AEFC", "AEG", "AEHL", "AEHR", "AEI", "AEIS", "AEL", "AEL^A", "AEL^B", "AEM",
+    "AEMD", "AENZ", "AEO", "AEP", "AEPPZ", "AER", "AES", "AESC", "AESI", "AEVA",
+    "AEY", "AEYE", "AEZS", "AFAR", "AFARU", "AFB", "AFBI", "AFCG", "AFG", "AFGB",
+    "AFGC", "AFGD", "AFGE", "AFIB", "AFL", "AFMD", "AFRI", "AFRIW", "AFRM", "AFT",
+    "AFTR"] 
 numeri=[1,2,3,4,5,6,7,8,9,10]
-for azione in azioni:
+regex = r'\d{2}-\d{4}'
+for azione in tqdm(azioni):
     print(f"{azione}")
 
     dizio_tot=[]
     
-    for num in numeri:
+    for num in tqdm(numeri):
         print(f"{azione} e pag {num}")
         if azione=="CHTR" and num==10:
             continue
@@ -57,96 +66,144 @@ for azione in azioni:
         print(len(new))
         lista=new
         lista2=[]
-        if (len(lista)-5)%6==0: #and len(lista)/6>20:
-            i=0
-            k=4
-            while i<=k:
-                lista2.append(lista[i])
-                i+=1
-            print(lista2)
-            for value in lista2:
-                lista.remove(value)
-            print(len(lista))
-            print("")
-            print(lista)
-            print("")
-            dic={}
-            i=0
-            if len(lista2)==5:
-                while i<=len(lista):
-                    dic.update({lista[i]:[lista[i+1],lista[i+2],lista[i+3],lista[i+4],lista[i+5]]})
-                    print(dic)
-                    i+=6
-                    if i==len(lista):
-                        break
-                print(dic)
-        elif (len(lista)-4)%5==0:
-            i=0
-            k=3
-            while i<=k:
-                lista2.append(lista[i])
-                i+=1
-            print(lista2)
-            for value in lista2:
-                lista.remove(value)
-            print(len(lista))
-            print("")
-            print(lista)
-            print("")
-            dic={}
-            i=0
-            if len(lista2)==4:
-                while i<=len(lista):
-                    dic.update({lista[i]:[lista[i+1],lista[i+2],lista[i+3],lista[i+4]]})
-                    i+=5
-                    if i==len(lista):
-                        break
-                print(dic)
+        for val in lista:
+            corrispondenze = re.findall(regex, val)
+            if corrispondenze:
+                lista2.extend(corrispondenze)
+        #print(lista2)
+        for val in lista2:
+            lista.remove(val)
+        #print(lista)
+        dic={}
+        i=0
+        if len(lista2)==5:
+            while i<=len(lista):
+                dic.update({lista[i]:[lista[i+1],lista[i+2],lista[i+3],lista[i+4],lista[i+5]]})
+                #print(dic)
+                i+=6
+                if i==len(lista):
+                    break
+        elif len(lista2)==4:
+            while i<=len(lista):
+                dic.update({lista[i]:[lista[i+1],lista[i+2],lista[i+3],lista[i+4]]})
+                i+=5
+                if i==len(lista):
+                    break
+        elif len(lista2)==3:
+            while i<=len(lista):
+                dic.update({lista[i]:[lista[i+1],lista[i+2],lista[i+3]]})
+                i+=4
+                if i==len(lista):
+                    break
+        elif len(lista2)==2:
+            while i<=len(lista):
+                dic.update({lista[i]:[lista[i+1],lista[i+2]]})
+                i+=3
+                if i==len(lista):
+                    break
+        else:
+            while i<=len(lista):
+                dic.update({lista[i]:[lista[i+1]]})
+                i+=2
+                if i==len(lista):
+                    break
 
-        elif (len(lista)-3)%4==0:
-            i=0
-            k=2
-            while i<=k:
-                lista2.append(lista[i])
-                i+=1
-            print(lista2)
-            for value in lista2:
-                lista.remove(value)
-            print(len(lista))
-            print("")
-            print(lista)
-            print("")
-            dic={}
-            i=0
-            if len(lista2)==3:
-                while i<=len(lista):
-                    dic.update({lista[i]:[lista[i+1],lista[i+2],lista[i+3]]})
-                    i+=4
-                    if i==len(lista):
-                        break
-                print(dic)
-        elif (len(lista)-2)%3==0:
-            i=0
-            k=1
-            while i<=k:
-                lista2.append(lista[i])
-                i+=1
-            print(lista2)
-            for value in lista2:
-                lista.remove(value)
-            print(len(lista))
-            print("")
-            print(lista)
-            print("")
-            dic={}
-            i=0
-            if len(lista2)==2:
-                while i<=len(lista):
-                    dic.update({lista[i]:[lista[i+1],lista[i+2]]})
-                    i+=3
-                    if i==len(lista):
-                        break
-                print(dic)
+
+        #print(dic)
+        # if len(new)<100:
+        #     continue
+        # lista=new
+        # lista2=[]
+        # if (len(lista)-5)%6==0 and (len(lista)-5)/6>14:
+        #     i=0
+        #     k=4
+        #     while i<=k:
+        #         lista2.append(lista[i])
+        #         i+=1
+        #     print(lista2)
+        #     for value in lista2:
+        #         lista.remove(value)
+        #     print(len(lista))
+        #     print("")
+        #     print(lista)
+        #     print("")
+        #     dic={}
+        #     i=0
+        #     if len(lista2)==5:
+        #         while i<=len(lista):
+        #             dic.update({lista[i]:[lista[i+1],lista[i+2],lista[i+3],lista[i+4],lista[i+5]]})
+        #             print(dic)
+        #             i+=6
+        #             if i==len(lista):
+        #                 break
+        #         print(dic)
+        # elif (len(lista)-4)%5==0:
+        #     i=0
+        #     k=3
+        #     while i<=k:
+        #         lista2.append(lista[i])
+        #         i+=1
+        #     print(lista2)
+        #     for value in lista2:
+        #         lista.remove(value)
+        #     print(len(lista))
+        #     print("")
+        #     print(lista)
+        #     print("")
+        #     dic={}
+        #     i=0
+        #     if len(lista2)==4:
+        #         while i<=len(lista):
+        #             dic.update({lista[i]:[lista[i+1],lista[i+2],lista[i+3],lista[i+4]]})
+        #             i+=5
+        #             if i==len(lista):
+        #                 break
+        #         print(dic)
+
+        # elif (len(lista)-3)%4==0:
+        #     i=0
+        #     k=2
+        #     while i<=k:
+        #         lista2.append(lista[i])
+        #         i+=1
+        #     print(lista2)
+        #     for value in lista2:
+        #         lista.remove(value)
+        #     print(len(lista))
+        #     print("")
+        #     print(lista)
+        #     print("")
+        #     dic={}
+        #     i=0
+        #     if len(lista2)==3:
+        #         while i<=len(lista):
+        #             dic.update({lista[i]:[lista[i+1],lista[i+2],lista[i+3]]})
+        #             i+=4
+        #             if i==len(lista):
+        #                 break
+        #         print(dic)
+        # elif (len(lista)-2)%3==0:
+        #     i=0
+        #     k=1
+        #     while i<=k:
+        #         lista2.append(lista[i])
+        #         i+=1
+        #     print(lista2)
+        #     for value in lista2:
+        #         lista.remove(value)
+        #     print(len(lista))
+        #     print("")
+        #     print(lista)
+        #     print("")
+        #     dic={}
+        #     i=0
+        #     if len(lista2)==2:
+        #         while i<=len(lista):
+        #             dic.update({lista[i]:[lista[i+1],lista[i+2]]})
+        #             i+=3
+        #             if i==len(lista):
+        #                 break
+        #         print(dic)
 
                     
 
@@ -164,6 +221,7 @@ for azione in azioni:
             continue
     fataframes.append(result)
     #result.to_csv(f"dati{azione}.csv")
+
 driver.quit()   
 fataframes_1=pd.concat(fataframes)
 print(fataframes_1)
@@ -171,4 +229,4 @@ fataframes_1=fataframes_1.reset_index(drop=True)
 print(fataframes_1)
 fataframes_1.to_csv("Balance_dati.csv")
 
-#acb pag lista2
+# #acb pag lista2
