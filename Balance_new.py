7a7df8bd7d83978737eb9b886d708f8c4e6d26c9
EnricoGiachetto <enrico.giachetto@rationence.eu>
2023-10-14 14:30:41 +0200

    Update Balance_new.py

--- Balance_new.py
+++ Balance_new.py
@@ -15,7 +15,7 @@ import re
 
 driver=webdriver.Firefox()
 fataframes=[]
-azioni=[ "AFTR", "AFYA", "AG", "AGAC", "AGAE", "AGBA", "AGBAW", "AGCO", "AGD",
+a1=["AFTR", "AFYA", "AG", "AGAC", "AGAE", "AGBA", "AGBAW", "AGCO", "AGD",
     "AGE", "AGEN", "AGFY", "AGI", "AGIL", "AGILW", "AGIO", "AGL", "AGLE",
     "AGM", "AGM^C", "AGM^D", "AGM^E", "AGM^F", "AGM^G", "AGMH", "AGNC",
     "AGNCL", "AGNCM", "AGNCN", "AGNCO", "AGNCP", "AGO", "AGR", "AGRI",
@@ -30,8 +30,8 @@ azioni=[ "AFTR", "AFYA", "AG", "AGAC", "AGAE", "AGBA", "AGBAW", "AGCO", "AGD",
     "ALAR", "ALB", "ALBT", "ALC", "ALCC", "ALCO", "ALCYU", "ALDX", "ALE",
     "ALEC", "ALEX", "ALG", "ALGM", "ALGN", "ALGS", "ALGT", "ALHC", "ALIM",
     "ALIT", "ALK", "ALKS", "ALKT", "ALL", "ALL^B", "ALL^H", "ALL^I", "ALLE",
-    "ALLG", "ALLK", "ALLO", "ALLR", "ALLT", "ALLY", "ALNY", "ALOR", "ALORU",
-    "ALOT", "ALPA", "ALPAU", "ALPAW", "ALPN", "ALPP", "ALPS", "ALRM", "ALRN",
+    "ALLG", "ALLK", "ALLO", "ALLR", "ALLT", "ALLY", "ALNY", "ALOR", "ALORU",]
+a2=[ "ALOT", "ALPA", "ALPAU", "ALPAW", "ALPN", "ALPP", "ALPS", "ALRM", "ALRN",
     "ALRS", "ALSA", "ALSAR", "ALSAW", "ALSN", "ALT", "ALTG", "ALTG^A", "ALTI",
     "ALTIW", "ALTO", "ALTR", "ALTU", "ALTUW", "ALV", "ALVO", "ALVR", "ALX",
     "ALXO", "ALYA", "ALZN", "AM", "AMAL", "AMAM", "AMAO", "AMAOU", "AMAT",
@@ -45,8 +45,8 @@ azioni=[ "AFTR", "AFYA", "AG", "AGAC", "AGAE", "AGBA", "AGBAW", "AGCO", "AGD",
     "ANGN", "ANGO", "ANIK", "ANIP", "ANIX", "ANNX", "ANPC", "ANSS", "ANTE",
     "ANTX", "ANVS", "ANY", "ANZU", "ANZUU", "ANZUW", "AOD", "AOGO", "AOGOU",
     "AOMR", "AON", "AORT", "AOS", "AOSL", "AOUT", "AP", "APA", "APAC",
-    "APACW", "APAM", "APCA", "APCX", "APCXW", "APD", "APDN", "APE", "APEI",
-    "APG", "APGB", "APGN", "APGNW", "APH", "API", "APLD", "APLE", "APLM",
+    "APACW", "APAM", "APCA", "APCX", "APCXW", "APD", "APDN", "APE", "APEI"]
+a3=["APG", "APGB", "APGN", "APGNW", "APH", "API", "APLD", "APLE", "APLM",
     "APLMW", "APLS", "APLT", "APM", "APMI", "APMIU", "APMIW", "APO", "APOG",
     "APP", "APPF", "APPH", "APPHW", "APPN", "APPS", "APRE", "APRN", "APT",
     "APTM", "APTMU", "APTMW", "APTO", "APTV", "APTV^A", "APTX", "APVO",
@@ -73,230 +73,226 @@ azioni=[ "AFTR", "AFYA", "AG", "AGAC", "AGAE", "AGBA", "AGBAW", "AGCO", "AGD",
     "ATRO", "ATSG", "ATTO", "ATUS", "ATVI", "ATXG", "ATXI", "ATXS", "AU",
     "AUB", "AUB^A", "AUBN"]
 
+azioni=[a1,a2,a3]
+
 numeri=[1,2,3,4,5,6,7,8,9,10]
 regex = r'\d{2}-\d{4}'
 driver.implicitly_wait(10)
 for azione in tqdm(azioni):
-    print(f"{azione}")
+	for a in azione:
+	    print(f"{azione}")
+	dizio_tot=[]   
+	    for num in numeri:
+	        print(f"{azione} e pag {num}")
+	       # driver.get(f"https://www.barchart.com/stocks/quotes/{azione}/balance-sheet/quarterly?reportPage={num}")
+	        try:
+	            WebDriverWait(driver, 3).until(EC.element_to_be_clickable((By.CSS_SELECTOR,"html body.hide-menu-for-landscape.add-ads-premier.modal-open div.reveal-modal.fade.bc-nui-auth-modal.bc-nui-auth-blue-modal.register-modal.authentication-modal.auth-no-modal-ref-class.in div div.bc-modal-login.bc-nui-modal-login.ng-scope div.form-close-wrapper.ng-scope i.bc-glyph-times.form-close"))).click()
+	        except:
+	        	pass
+	        try:
+	            WebDriverWait(driver, 3).until(EC.element_to_be_clickable((By.CSS_SELECTOR,"html body.hide-menu-for-landscape.add-ads-premier div.a__sc-np32r2-0.eqYWHN div.Card-sc-1s2p2gv-0.a__sc-3vtlsk-0.kDNyTh.jXixFa div.Card__CardHeader-sc-1s2p2gv-1.a__sc-3vtlsk-1.fZGtpv.cVIXeq div.Frame-sc-1d4hofp-0.fRUcSy button.Button__StyledButton-a1qza5-0.jkvvVr"))).click()
+	        except:
+	        	pass
+	       # time.sleep(3)
+	        button=driver.find_element(By.CSS_SELECTOR, "html body.hide-menu-for-landscape.add-ads-premier main#bc-main-content-wrapper.off-canvas-wrap.ng-isolate-scope div.inner-wrap div.main-content-wrapper.content.js-main-content-wrapper div.row div.large-12.columns div.one-column-block div.row div#main-content-column.small-12.columns.main-column div.column-inner div.bc-financial-report div.bc-financial-report__pagination.clearfix div.right a.bc-button.light-blue.ok").text
+	        print(button)
+	        if (num==1):
+
+	        	pass
+	        elif (button!="NEXT"):
+	        	break
+	        else:
+
+	        	try:
+
+	        		WebDriverWait(driver, 3).until(EC.element_to_be_clickable((By.CSS_SELECTOR,"html body.hide-menu-for-landscape.add-ads-premier main#bc-main-content-wrapper.off-canvas-wrap.ng-isolate-scope div.inner-wrap div.main-content-wrapper.content.js-main-content-wrapper div.row div.large-12.columns div.one-column-block div.row div#main-content-column.small-12.columns.main-column div.column-inner div.bc-financial-report div.bc-financial-report__pagination.clearfix div.right a.bc-button.light-blue.ok"))).click()
+	        	except:
+	        		continue
 
 
-    try:
-    	driver.get(f"https://www.barchart.com/stocks/quotes/{azione}/balance-sheet/quarterly?reportPage")
-    except:
-    	continue
-    dizio_tot=[]
-    
-    for num in numeri:
-        print(f"{azione} e pag {num}")
-       # driver.get(f"https://www.barchart.com/stocks/quotes/{azione}/balance-sheet/quarterly?reportPage={num}")
-        try:
-            WebDriverWait(driver, 3).until(EC.element_to_be_clickable((By.CSS_SELECTOR,"html body.hide-menu-for-landscape.add-ads-premier.modal-open div.reveal-modal.fade.bc-nui-auth-modal.bc-nui-auth-blue-modal.register-modal.authentication-modal.auth-no-modal-ref-class.in div div.bc-modal-login.bc-nui-modal-login.ng-scope div.form-close-wrapper.ng-scope i.bc-glyph-times.form-close"))).click()
-        except:
-        	pass
-        try:
-            WebDriverWait(driver, 3).until(EC.element_to_be_clickable((By.CSS_SELECTOR,"html body.hide-menu-for-landscape.add-ads-premier div.a__sc-np32r2-0.eqYWHN div.Card-sc-1s2p2gv-0.a__sc-3vtlsk-0.kDNyTh.jXixFa div.Card__CardHeader-sc-1s2p2gv-1.a__sc-3vtlsk-1.fZGtpv.cVIXeq div.Frame-sc-1d4hofp-0.fRUcSy button.Button__StyledButton-a1qza5-0.jkvvVr"))).click()
-        except:
-        	pass
-        time.sleep(3)
-        button=driver.find_element(By.CSS_SELECTOR, "html body.hide-menu-for-landscape.add-ads-premier main#bc-main-content-wrapper.off-canvas-wrap.ng-isolate-scope div.inner-wrap div.main-content-wrapper.content.js-main-content-wrapper div.row div.large-12.columns div.one-column-block div.row div#main-content-column.small-12.columns.main-column div.column-inner div.bc-financial-report div.bc-financial-report__pagination.clearfix div.right a.bc-button.light-blue.ok").text
-        print(button)
-        if (num==1):
-
-        	pass
-        elif (button!="NEXT"):
-        	break
-        else:
-
-        	try:
-
-        		WebDriverWait(driver, 3).until(EC.element_to_be_clickable((By.CSS_SELECTOR,"html body.hide-menu-for-landscape.add-ads-premier main#bc-main-content-wrapper.off-canvas-wrap.ng-isolate-scope div.inner-wrap div.main-content-wrapper.content.js-main-content-wrapper div.row div.large-12.columns div.one-column-block div.row div#main-content-column.small-12.columns.main-column div.column-inner div.bc-financial-report div.bc-financial-report__pagination.clearfix div.right a.bc-button.light-blue.ok"))).click()
-        	except:
-        		continue
+	                #time.sleep(3)
 
 
-                #time.sleep(3)
+	        values=driver.find_elements(By.CSS_SELECTOR, "html body.hide-menu-for-landscape.add-ads-premier main#bc-main-content-wrapper.off-canvas-wrap.ng-isolate-scope div.inner-wrap div.main-content-wrapper.content.js-main-content-wrapper div.row div.large-12.columns div.one-column-block div.row div#main-content-column.small-12.columns.main-column div.column-inner div.bc-financial-report div.bc-table-scrollable div.bc-table-scrollable-inner ng-transclude table.ng-scope tbody tr td")
+
+	        print(len(values))
+	        lista=[]
+	        for value in values:
+	            #print(value.text)
+	            lista.append(value.text)
+	       # print(lista)
+	        if len(lista)==0:
+	            break
+	        new = []
+	        for val in lista:
+	            if val.strip() != '' and val != 'Current Assets' and val != "Non-Current Assets" and val != "Current Liabilities" and val != "Non-Current Liabilities" and val!='ASSETS' and val!='LIABILITIES'and val!="SHAREHOLDERS' EQUITY":
+	                new.append(val)
+
+	        #print(new)
+	        #print(len(new))
+	        lista=new
+	        lista2=[]
+	        for val in lista:
+	            corrispondenze = re.findall(regex, val)
+	            if corrispondenze:
+	                lista2.extend(corrispondenze)
+	        #print(lista2)
+	        for val in lista2:
+	            lista.remove(val)
+	        #print(lista)
+	        dic={}
+	        i=0
+	        if len(lista2)==5:
+	            while i<=len(lista):
+	                dic.update({lista[i]:[lista[i+1],lista[i+2],lista[i+3],lista[i+4],lista[i+5]]})
+	                #print(dic)
+	                i+=6
+	                if i==len(lista):
+	                    break
+	        elif len(lista2)==4:
+	            while i<=len(lista):
+	                dic.update({lista[i]:[lista[i+1],lista[i+2],lista[i+3],lista[i+4]]})
+	                i+=5
+	                if i==len(lista):
+	                    break
+	        elif len(lista2)==3:
+	            while i<=len(lista):
+	                dic.update({lista[i]:[lista[i+1],lista[i+2],lista[i+3]]})
+	                i+=4
+	                if i==len(lista):
+	                    break
+	        elif len(lista2)==2:
+	            while i<=len(lista):
+	                dic.update({lista[i]:[lista[i+1],lista[i+2]]})
+	                i+=3
+	                if i==len(lista):
+	                    break
+	        else:
+	            while i<=len(lista):
+	                dic.update({lista[i]:[lista[i+1]]})
+	                i+=2
+	                if i==len(lista):
+	                    break
 
 
-        values=driver.find_elements(By.CSS_SELECTOR, "html body.hide-menu-for-landscape.add-ads-premier main#bc-main-content-wrapper.off-canvas-wrap.ng-isolate-scope div.inner-wrap div.main-content-wrapper.content.js-main-content-wrapper div.row div.large-12.columns div.one-column-block div.row div#main-content-column.small-12.columns.main-column div.column-inner div.bc-financial-report div.bc-table-scrollable div.bc-table-scrollable-inner ng-transclude table.ng-scope tbody tr td")
+	        #print(dic)
+	        # if len(new)<100:
+	        #     continue
+	        # lista=new
+	        # lista2=[]
+	        # if (len(lista)-5)%6==0 and (len(lista)-5)/6>14:
+	        #     i=0
+	        #     k=4
+	        #     while i<=k:
+	        #         lista2.append(lista[i])
+	        #         i+=1
+	        #     print(lista2)
+	        #     for value in lista2:
+	        #         lista.remove(value)
+	        #     print(len(lista))
+	        #     print("")
+	        #     print(lista)
+	        #     print("")
+	        #     dic={}
+	        #     i=0
+	        #     if len(lista2)==5:
+	        #         while i<=len(lista):
+	        #             dic.update({lista[i]:[lista[i+1],lista[i+2],lista[i+3],lista[i+4],lista[i+5]]})
+	        #             print(dic)
+	        #             i+=6
+	        #             if i==len(lista):
+	        #                 break
+	        #         print(dic)
+	        # elif (len(lista)-4)%5==0:
+	        #     i=0
+	        #     k=3
+	        #     while i<=k:
+	        #         lista2.append(lista[i])
+	        #         i+=1
+	        #     print(lista2)
+	        #     for value in lista2:
+	        #         lista.remove(value)
+	        #     print(len(lista))
+	        #     print("")
+	        #     print(lista)
+	        #     print("")
+	        #     dic={}
+	        #     i=0
+	        #     if len(lista2)==4:
+	        #         while i<=len(lista):
+	        #             dic.update({lista[i]:[lista[i+1],lista[i+2],lista[i+3],lista[i+4]]})
+	        #             i+=5
+	        #             if i==len(lista):
+	        #                 break
+	        #         print(dic)
 
-        print(len(values))
-        lista=[]
-        for value in values:
-            #print(value.text)
-            lista.append(value.text)
-       # print(lista)
-        if len(lista)==0:
-            break
-        new = []
-        for val in lista:
-            if val.strip() != '' and val != 'Current Assets' and val != "Non-Current Assets" and val != "Current Liabilities" and val != "Non-Current Liabilities" and val!='ASSETS' and val!='LIABILITIES'and val!="SHAREHOLDERS' EQUITY":
-                new.append(val)
+	        # elif (len(lista)-3)%4==0:
+	        #     i=0
+	        #     k=2
+	        #     while i<=k:
+	        #         lista2.append(lista[i])
+	        #         i+=1
+	        #     print(lista2)
+	        #     for value in lista2:
+	        #         lista.remove(value)
+	        #     print(len(lista))
+	        #     print("")
+	        #     print(lista)
+	        #     print("")
+	        #     dic={}
+	        #     i=0
+	        #     if len(lista2)==3:
+	        #         while i<=len(lista):
+	        #             dic.update({lista[i]:[lista[i+1],lista[i+2],lista[i+3]]})
+	        #             i+=4
+	        #             if i==len(lista):
+	        #                 break
+	        #         print(dic)
+	        # elif (len(lista)-2)%3==0:
+	        #     i=0
+	        #     k=1
+	        #     while i<=k:
+	        #         lista2.append(lista[i])
+	        #         i+=1
+	        #     print(lista2)
+	        #     for value in lista2:
+	        #         lista.remove(value)
+	        #     print(len(lista))
+	        #     print("")
+	        #     print(lista)
+	        #     print("")
+	        #     dic={}
+	        #     i=0
+	        #     if len(lista2)==2:
+	        #         while i<=len(lista):
+	        #             dic.update({lista[i]:[lista[i+1],lista[i+2]]})
+	        #             i+=3
+	        #             if i==len(lista):
+	        #                 break
+	        #         print(dic)
 
-        #print(new)
-        #print(len(new))
-        lista=new
-        lista2=[]
-        for val in lista:
-            corrispondenze = re.findall(regex, val)
-            if corrispondenze:
-                lista2.extend(corrispondenze)
-        #print(lista2)
-        for val in lista2:
-            lista.remove(val)
-        #print(lista)
-        dic={}
-        i=0
-        if len(lista2)==5:
-            while i<=len(lista):
-                dic.update({lista[i]:[lista[i+1],lista[i+2],lista[i+3],lista[i+4],lista[i+5]]})
-                #print(dic)
-                i+=6
-                if i==len(lista):
-                    break
-        elif len(lista2)==4:
-            while i<=len(lista):
-                dic.update({lista[i]:[lista[i+1],lista[i+2],lista[i+3],lista[i+4]]})
-                i+=5
-                if i==len(lista):
-                    break
-        elif len(lista2)==3:
-            while i<=len(lista):
-                dic.update({lista[i]:[lista[i+1],lista[i+2],lista[i+3]]})
-                i+=4
-                if i==len(lista):
-                    break
-        elif len(lista2)==2:
-            while i<=len(lista):
-                dic.update({lista[i]:[lista[i+1],lista[i+2]]})
-                i+=3
-                if i==len(lista):
-                    break
-        else:
-            while i<=len(lista):
-                dic.update({lista[i]:[lista[i+1]]})
-                i+=2
-                if i==len(lista):
-                    break
+	                    
 
+	        dic1={"year":lista2}
+	        dic1.update(dic)
+	        df = pd.DataFrame(dic1,index=lista2)
+	        df=df.assign(Azione=f"{azione}")
+	                #df.drop(df.columns[[0]], axis = 1)
+	        dizio_tot.append(df)
 
-        #print(dic)
-        # if len(new)<100:
-        #     continue
-        # lista=new
-        # lista2=[]
-        # if (len(lista)-5)%6==0 and (len(lista)-5)/6>14:
-        #     i=0
-        #     k=4
-        #     while i<=k:
-        #         lista2.append(lista[i])
-        #         i+=1
-        #     print(lista2)
-        #     for value in lista2:
-        #         lista.remove(value)
-        #     print(len(lista))
-        #     print("")
-        #     print(lista)
-        #     print("")
-        #     dic={}
-        #     i=0
-        #     if len(lista2)==5:
-        #         while i<=len(lista):
-        #             dic.update({lista[i]:[lista[i+1],lista[i+2],lista[i+3],lista[i+4],lista[i+5]]})
-        #             print(dic)
-        #             i+=6
-        #             if i==len(lista):
-        #                 break
-        #         print(dic)
-        # elif (len(lista)-4)%5==0:
-        #     i=0
-        #     k=3
-        #     while i<=k:
-        #         lista2.append(lista[i])
-        #         i+=1
-        #     print(lista2)
-        #     for value in lista2:
-        #         lista.remove(value)
-        #     print(len(lista))
-        #     print("")
-        #     print(lista)
-        #     print("")
-        #     dic={}
-        #     i=0
-        #     if len(lista2)==4:
-        #         while i<=len(lista):
-        #             dic.update({lista[i]:[lista[i+1],lista[i+2],lista[i+3],lista[i+4]]})
-        #             i+=5
-        #             if i==len(lista):
-        #                 break
-        #         print(dic)
+	    if len(dizio_tot)>0:
+	        result=pd.concat(dizio_tot)
+	        print(result)
+	    else:
+	            continue
+	    fataframes.append(result)
+	    #result.to_csv(f"dati{azione}.csv")
 
-        # elif (len(lista)-3)%4==0:
-        #     i=0
-        #     k=2
-        #     while i<=k:
-        #         lista2.append(lista[i])
-        #         i+=1
-        #     print(lista2)
-        #     for value in lista2:
-        #         lista.remove(value)
-        #     print(len(lista))
-        #     print("")
-        #     print(lista)
-        #     print("")
-        #     dic={}
-        #     i=0
-        #     if len(lista2)==3:
-        #         while i<=len(lista):
-        #             dic.update({lista[i]:[lista[i+1],lista[i+2],lista[i+3]]})
-        #             i+=4
-        #             if i==len(lista):
-        #                 break
-        #         print(dic)
-        # elif (len(lista)-2)%3==0:
-        #     i=0
-        #     k=1
-        #     while i<=k:
-        #         lista2.append(lista[i])
-        #         i+=1
-        #     print(lista2)
-        #     for value in lista2:
-        #         lista.remove(value)
-        #     print(len(lista))
-        #     print("")
-        #     print(lista)
-        #     print("")
-        #     dic={}
-        #     i=0
-        #     if len(lista2)==2:
-        #         while i<=len(lista):
-        #             dic.update({lista[i]:[lista[i+1],lista[i+2]]})
-        #             i+=3
-        #             if i==len(lista):
-        #                 break
-        #         print(dic)
-
-                    
-
-        dic1={"year":lista2}
-        dic1.update(dic)
-        df = pd.DataFrame(dic1,index=lista2)
-        df=df.assign(Azione=f"{azione}")
-                #df.drop(df.columns[[0]], axis = 1)
-        dizio_tot.append(df)
-
-    if len(dizio_tot)>0:
-        result=pd.concat(dizio_tot)
-        print(result)
-    else:
-            continue
-    fataframes.append(result)
-    #result.to_csv(f"dati{azione}.csv")
-
-driver.quit()   
-fataframes_1=pd.concat(fataframes)
-print(fataframes_1)
-fataframes_1=fataframes_1.reset_index(drop=True)
-print(fataframes_1)
-fataframes_1.to_csv("Balance_dati200_700.csv")
+	driver.quit()   
+	fataframes_1=pd.concat(fataframes)
+	print(fataframes_1)
+	fataframes_1=fataframes_1.reset_index(drop=True)
+	print(fataframes_1)
+	fataframes_1.to_csv(f"Balance_dati{azione}.csv")
 
 # #acb pag lista2
